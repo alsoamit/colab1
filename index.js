f6a55b61bbbf152c6afd1e1cdee0a4539c43d40a
Pushkar Anand <pushkar.anand@learner.manipal.edu>
2021-09-03 16:05:20 +0530

    version 1 completed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let spans = [document.querySelector(".span1"), document.querySelector(".span2"), document.querySelector(".span3")]
+let spans = [document.querySelector(".toggler1"), document.querySelector(".toggler2"), document.querySelector(".toggler3")];
+let hamb = document.querySelector(".hamb");
 
 spans.map((span)=>{
     span.addEventListener("click", ()=>{
@@ -9,5 +10,13 @@ spans.map((span)=>{
     })
 })
 
-
-
+hamb.addEventListener("click", ()=>{
+    if (hamb.classList.contains("open")){
+        hamb.classList.remove("open");
+        document.querySelector(".overlay").style.display = "none";
+    }
+    else{
+        hamb.classList.add("open");
+        document.querySelector(".overlay").style.display = "block";
+    }
+})
